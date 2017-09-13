1a8d63206791a4fa5e5be71caffc40b5d6c9e024
Racter <paracetamolboy@gmail.com>
2017-09-13 14:26:07 +0200

    Make django-vaultkeeper-adaptor read the file as well.

--- django_vaultkeeper_adaptor/tests/test_adaptor.py
+++ django_vaultkeeper_adaptor/tests/test_adaptor.py
@@ -4,58 +4,20 @@ from ..vaultkeeper_adaptor import VKAdaptor
 class TestAdaptor(object):
 
     def setup(self):
-        self.data = [
-            {
-                'id': 'default',
-                'backend': 'postgresql',
-                'endpoint': '0.0.0.0:5432/mydb',
-                'vault_path': 'database/creds/psql-rw',
-                'schema': 'public',
-                'policy': 'psql-rw',
-                'set_role': 'app_owner',
-                'username': 'ptest',
-                'password': 'ppass',
-                'lease_duration': 100,
-                'renewable': True,
-            },
-            {
-                'id': 'auxiliary',
-                'backend': 'postgresql',
-                'endpoint': '1.1.1.1:5432/mydb1',
-                'vault_path': 'database/creds/psql-rw-ax',
-                'schema': 'public',
-                'policy': 'psql-rw-ax',
-                'set_role': 'app_owner1',
-                'username': 'ptest1',
-                'password': 'ppass1',
-                'lease_duration': 100,
-                'renewable': True,
-            },
-            {
-                'id': 'broker1',
-                'backend': 'rabbitmq',
-                'endpoint': '0.0.0.0:5672/myvhost',
-                'vault_path': '/rabbitmq/creds/ampq-worker',
-                'username': 'rtest',
-                'password': 'rpass',
-                'vhost': 'myvhost',
-                'policy': 'ampq-worker',
-            }
-        ]
         self.DATABASES = {}
         self.BROKER_URL = ''
-        self.adaptor = VKAdaptor(data=self.data,
+        self.adaptor = VKAdaptor(config_path='./test/testconfig.json',
                                  DATABASES=self.DATABASES,
                                  BROKER_URL=self.BROKER_URL)
 
     def test_get_broker_url(self):
-        broker = self.data[2]
+        broker = self.adaptor.data[2]
         self.adaptor.get_broker_url(broker)
         assert (self.adaptor.BROKER_URL ==
                 'amqp://rtest:rpass@0.0.0.0:5672/myvhost')
 
     def test_get_database_configs(self):
-        db = self.data[0]
+        db = self.adaptor.data[0]
         self.adaptor.get_database_configs(db)
         assert self.adaptor.DATABASES == {
              'default': {
--- django_vaultkeeper_adaptor/vaultkeeper_adaptor.py
+++ django_vaultkeeper_adaptor/vaultkeeper_adaptor.py
@@ -1,4 +1,5 @@
 import dj_database_url
+import json
 
 
 def build_ids_url(backend, endpoint, identifier, secret):
@@ -13,20 +14,27 @@ def build_ids_url(backend, endpoint, identifier, secret):
 class VKAdaptor(object):
 
     def __init__(self,
-                 data=None,
+                 config_path=None,
                  DATABASES=None,
                  BROKER_URL=None,
                  ):
+        self.config_path = config_path
         self.DATABASES = DATABASES
         self.BROKER_URL = BROKER_URL
+        self.secrets = {}
 
         self.functionmap = {
             'postgresql': self.get_database_configs,
             'rabbitmq': self.get_broker_url,
         }
 
-        self.data = data
-        self.secrets = {}
+        try:
+            with open(self.config_path) as f:
+                data = json.load(f)
+                self.data = data
+        except IOError:
+            raise IOError("Error: File {} not found.".format(
+                          self.config_path))
 
     def process_all(self):
         for entry in self.data:
